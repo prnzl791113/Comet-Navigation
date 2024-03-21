936b4cfaf6fa9c92818da90877e0a2b36cedcde7
prnzl791113 <113567108+prnzl791113@users.noreply.github.com>
2024-03-21 14:59:18 -0500

    new commit

---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
+
+public class Location : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshProUGUI info;
+    string last;
+
+    void Start()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            return;
+        }
+        last = "";
+
+        Input.location.Start(1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        info.text = Input.location.lastData.latitude.ToString()+Input.location.lastData.longitude;
+        ExecuteAfterDelay(5);
+    }
+    IEnumerator ExecuteAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // Code to execute after the delay
+        Debug.Log("Code executed after 5 seconds.");
+    }
+}
